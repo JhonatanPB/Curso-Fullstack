52cb7a0bd721e7a242976ef7e15de980e604faca
JhonatanPB <felixjhonatan250886p@gmail.com>
2026-05-19 14:58:56 -0300

    criando rotas

--- kPfgi/app.js
+++ kPfgi/app.js
@@ -2,11 +2,35 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//Variável com os dados
+const alimentos = [
+    {id: 1, nome: "Feijão", preco: 10},
+    {id: 2, nome: "Carne", preco: 50},
+    {id: 3, nome: "Frutas", preco: 70},
+    {id: 4, nome: "Sopas", preco: 40}
+];
+
+const tiposPratos = [
+    {id: 1, nome: "lasanha", preco: 20},
+    {id: 2, nome: "panquecas", preco: 15},
+    {id: 3, nome: "carne-moida", preco: 10},
+    {id: 4, nome: "miojo", preco: 5}
+]
+
 // Criar uma função
 // Rota principal
 app.get('/', (req, res) => {
     res.send("Bem vindo ao meu pojeto de Alimentos");
 });
+
+// Produtos
+app.get('/produtos', (req, res) => {
+    res.send(alimentos);
+})
+
+app.get('/tiposPratos', (req, res) => {
+    res.send(tiposPratos);
+})
 
 //Executando o servidor 
 app.listen(port, () => {
